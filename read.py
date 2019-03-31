d2b871f4fd47bd88b47a27d38a127397af6d9839
itshoom <hoomstory@naver.com>
2019-03-31 22:06:54 +0900

    final+1

--- read.py
+++ read.py
@@ -4,22 +4,31 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 10000 == 0:
+        if count % 100000 == 0:
             print(len(data))
-print('档案读取完了，总共有: ', len(data), '笔资料')
-sum_len = 0
-for d in data:
-    sum_len += len(d)
-print('每笔留言的平均长度是: ', sum_len / len(data))
-new = []
-for d in data:
-    if len(d) < 100:
-        new.append(d)
-print('一共有', len(new), '笔留言长度小于100')
-print(new[0])
-good = []
-for d in data:
-    if 'good' in d:
-        good.append(d)
-print('一共有', len(good), '笔留言提到good')
-print(good[0])
+print('档案读取完了，总共有', len(data), '笔资料')
+
+sum_line = 0 # 平均长度=总长度/个数
+for x in data:
+    sum_line += len(x)
+print('留言平均长度为: ', sum_line / len(data))
+
+#y = []
+#for d in data:
+    #if len(d) < 100:
+        #y.append(d)
+
+y = [d for d in data if len(d) < 100]   
+print('共有', len(y), '条留言少于100字')
+
+#bad = []
+#for q in data:
+    #if 'bad' in q:
+        #bad.append(q)
+#print(len(bad))
+#print(bad[5])
+
+bad = [q for q in data if 'bad' in q]
+print('提到bad的留言有', len(bad), '条')
+print('例如\n', bad[3])
+
