3f71e681ddf4c2f37ad42396f84c2080ed91a27f
itshoom <hoomstory@naver.com>
2019-03-31 15:52:09 +0900

    calc avg

--- read.py
+++ read.py
@@ -4,9 +4,10 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
+        if count % 10000 == 0:
             print(len(data))
-print(len(data))
-print(data[0])
-print('--------------------')
-print(data[1])
+print('档案读取完了，总共有: ', len(data), '笔资料')
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('每笔留言的平均长度是: ', sum_len / len(data))
