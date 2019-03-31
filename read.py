22153e9cb5e55313ee86bf7f05a944199e7d8fd8
itshoom <hoomstory@naver.com>
2019-03-31 16:27:32 +0900

    final

--- read.py
+++ read.py
@@ -11,3 +11,15 @@ sum_len = 0
 for d in data:
     sum_len += len(d)
 print('每笔留言的平均长度是: ', sum_len / len(data))
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '笔留言长度小于100')
+print(new[0])
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '笔留言提到good')
+print(good[0])
